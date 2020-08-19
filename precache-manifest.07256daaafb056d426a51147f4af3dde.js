3af714c508f5faa48d232e49d7d2e1d63cde12ed
AlexeyGorbachevskiy <alexeygorbachevskiyy@gmail.com>
2020-08-19 21:06:05 +0300

    Updates

---
+++ precache-manifest.07256daaafb056d426a51147f4af3dde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dc1c175cd37c3f7e13ef508ec700b2c",
+    "url": "/PartyCards/index.html"
+  },
+  {
+    "revision": "33d70980fa5e19b0b772",
+    "url": "/PartyCards/static/css/main.d5061ec4.chunk.css"
+  },
+  {
+    "revision": "d77c27c5ab8c01521a16",
+    "url": "/PartyCards/static/js/2.535c365d.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/PartyCards/static/js/2.535c365d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33d70980fa5e19b0b772",
+    "url": "/PartyCards/static/js/main.7293d155.chunk.js"
+  },
+  {
+    "revision": "db8fd9aa6aaa71974370",
+    "url": "/PartyCards/static/js/runtime-main.810ffe53.js"
+  }
+]);
